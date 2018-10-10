a20d6a65a814df02db65b40106bce573184ac7f6
rrodriguez <rrodriguez@dude-monkey.com>
2018-10-10 18:11:05 +0200

    Add post to do route and refactor moongose

---
+++ server/db/mongoose.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp'); 
+
+
+
+// var newTodo = new Todo({
+//     text: true
+// })
+
+// var newTodo = new Todo({
+//     text: 'Buy groceries',
+//     completed: true,
+//     completedAt: 123   
+// });
+
+// newTodo.save().then( (doc) => {
+//     console.log('Saved todo', doc);
+// }, (e) => {
+//     console.log('unable to save todo');
+// });
+
+
+
+// var newUser = new User({
+//     email: 'myemail@mail.com   '
+// });
+
+// newUser.save().then( (doc) => {
+//     console.log('Saved user:', doc);
+// }, (e) => {
+//     console.log('Unable to save user:', e);
+// });
+
+module.exports = {
+    mongoose: mongoose
+};
