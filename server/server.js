f9b20bc71c75ddd2492aaef7fa97fc2600f8571c
rrodriguez <rrodriguez@dude-monkey.com>
2018-10-31 17:44:23 +0100

    Setup separate and development env

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-const _ = require('lodash')
+require('.././config/config');
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const ObjectID = require('mongodb').ObjectID;
