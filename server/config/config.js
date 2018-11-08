9d76fbdf8ec73c1170e19d5eb3ee45c823fc9ad9
rrodriguez <rrodriguez@dude-monkey.com>
2018-11-08 12:25:23 +0100

    Config modified

---
+++ server/config/config.js
@@ -0,0 +1,16 @@
+var env = process.env.NODE_ENV || "development";
+console.log("**** env", env);
+
+var config = require('./config.json');
+var envConfig = config[env];
+Object.keys(envConfig).forEach( (key) => {
+    process.env[key] = envConfig[key];
+});
+
+// if(env === "development"){
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// }else if( env === "test"){
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }else if( env == "production"){
+//     process.env.MONGODB_URI = 'mongodb://dudemonkey:dudemonkeyT3stDB@ds241723.mlab.com:41723/todo-api-db';
+// }
